d77439b866de69060e4116286752161987b42684
ruhollahmozafari <ruhollahmozafari@gmail.com>
2022-01-07 22:24:59 +0330

    added the livereload for templates

--- uni/settings.py
+++ uni/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'movie',
     'scrapy_app',
     'django_elasticsearch_dsl',
+    'livereload',
 ] 
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'uni.urls'
