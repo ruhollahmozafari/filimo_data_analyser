b66267914985b493ca7a45356dd53b9ed7bc449b
ruhollahmozafari <ruhollahmozafari@gmail.com>
2022-02-07 01:13:26 +0330

    set up data for rating

--- movie/models.py
+++ movie/models.py
@@ -63,8 +63,16 @@ class Movie(models.Model):
        for x in comment_obj:
            comments.append({'id': x.id, 'fname': x.fname, 'ename': x.ename})
        return comments
-    def number_of_minus_one(self):
-        return self.sentiment_detail.get(-1,0)
+    def number_very_bad_comment(self):
+        return self.sentiment_detail.get('-1',0)
+    def number_bad_comment(self):
+        return self.sentiment_detail.get('-0.5',0)
+    def number_good_comment(self):
+        return self.sentiment_detail.get('0.5',0)
+    def number_very_good_comment(self):
+        return self.sentiment_detail.get('1',0)
+    def number_of_zero_comment(self):
+        return self.sentiment_detail.get('0',0)
 class Comment(models.Model):
     movie = models.ForeignKey(Movie, blank= True, null= True, on_delete=models.CASCADE, related_name='comment_movie')
     text = models.TextField(blank=True, null= True , max_length=1000)
