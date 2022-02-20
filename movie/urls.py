538115d3c0c33247175a6c4fb881e443e2e7c2b3
ruhollahmozafari <ruhollahmzoafari@gmail.com>
2022-02-20 23:34:15 +0330

    restore the removed dir movie

---
+++ movie/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name='movie' 
+urlpatterns = [
+    path("list/", ListMovie.as_view(), name = 'home'),
+    path("", ListMovie.as_view(), name = 'home'),
+    path('search_result/', SearchResultView.as_view(), name='search-results'),
+    path('detail/<int:pk>/',MoviePageView.as_view(), name = 'movie_page' ),
+    # path('emotion/', GetPolarity.as_view())
+]
