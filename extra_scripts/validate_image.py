327dc4a027fb6d5fe3b60e1a1fec28557e41717f
ruhollahmozafari <ruhollahmzoafari@gmail.com>
2022-02-24 00:07:09 +0330

    fixed the structor of directories

--- extra_scripts/validate_image.py
+++
@@ -1,7 +0,0 @@
-from extra_scripts.EMS import validation_error
-
-
-def validate_image(image, megabyte_limit=1):
-    ImageSize = image.file.size
-    if ImageSize > megabyte_limit*1024*1024:
-        raise validation_error("Max file size is %sMB" % str(megabyte_limit))
